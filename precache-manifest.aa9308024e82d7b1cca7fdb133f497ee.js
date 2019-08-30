db2a7cb1458efce7329037340da0fb701467c4dc
Julian Burr <julian.burr@rexsoftware.com.au>
2019-08-30 15:46:12 +1000

    Updates

---
+++ precache-manifest.aa9308024e82d7b1cca7fdb133f497ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702e108f39b8b4dd557b44aa31c128cf",
+    "url": "/breakout-react/index.html"
+  },
+  {
+    "revision": "e25dbca6a731c558794f",
+    "url": "/breakout-react/static/js/2.9c20abf9.chunk.js"
+  },
+  {
+    "revision": "5de928142db2b991f064",
+    "url": "/breakout-react/static/js/main.87f7ff53.chunk.js"
+  },
+  {
+    "revision": "914933d7ffbdffdafec6",
+    "url": "/breakout-react/static/js/runtime~main.d02f7236.js"
+  }
+]);
